37abc9445b229b33172afc4d44391b92fc73b3f5
Honza Jedlicka <yedlosh@gmail.com>
2017-05-15 23:11:04 +0200

    Add Redux

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
+import reduxThunk from 'redux-thunk';
+import {Provider} from "react-redux";
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 import Main from "./components/Main";
+import reducers from './state/reducers';
+
+const createStoreWithMiddleware = composeWithDevTools(applyMiddleware(reduxThunk))(createStore);
+export const store = createStoreWithMiddleware(reducers);
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
@@ -11,7 +19,9 @@ injectTapEventPlugin();
 
 // Render the main app react component into the app div
 render((
+  <Provider store={store}>
     <Router>
       <Main />
     </Router>
+  </Provider>
 ), document.getElementById('app'));
